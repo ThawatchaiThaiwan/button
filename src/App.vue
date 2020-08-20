5c62ece305eecabad5adba806c3e61be096805d1
suchanun6104101393 <zadkee27@gmail.com>
2020-08-20 15:16:16 +0700

    1

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div id="app">
+  <img alt="Vue logo" src="./assets/logo.jpg" />
+    <HelloWorld msg="Thawatchai" />
+    <hr>
+    <div class="row">
+      <div class="col-6">
+    <img alt="Vue logo" src="https://thelistlove.com/wp-content/uploads/2015/07/Royal-Flush.gif" width="500" height="281" />
+        <h1>{{total1}}</h1>
+      </div>
+      <div class="col-6">
+
+<img alt="Vue logo" src="https://media0.giphy.com/media/3oeHLrjZGBgnPx5VII/giphy.gif" />
+        <h1>{{total2}}</h1>
+      </div>
+      <div class="col-6">
+        <CsButton @cc="sum" />
+      </div>
+      <div class="col-6">
+        <CsButton @cc="sum1" />
+      </div>
+    </div>
+    
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import CsButton from "./components/CSButton.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    CsButton,
+    
+  },
+  data : function(){
+    return {
+    total1: 0,
+    total2: 0,
+   
+   } 
+  },
+  methods: {
+  sum(counterValue){
+    this.total1 = counterValue
+  },
+  
+  sum1(counterValue){
+    this.total2 = counterValue
+ 
+  
+},
+},
+
+};
+
 </script>
 
 <style>
@@ -22,7 +62,9 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #0b44ff;
   margin-top: 60px;
+  background: url(https://images.wallpapersden.com/image/download/line-colorful-bright_4075_2560x1600.jpg) no-repeat center fixed;
+  background-size: cover;
 }
 </style>
---
+++ src/components/CSButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <button v-on:click="upFunction" class="btn btn-danger">ขอเป็นทาส {{lable}}</button>
+    <button v-on:click="downFunction" class="btn btn-warning ml-3">ไม่อยากเป็นทาส {{lable}}</button>
+    <button v-on:click="LoveFunction" class="btn btn-success ml-3">ชอบแต่เลี้ยงไม่ได้ {{lable}}</button>
+    <button v-on:click="ResetFunction" class="btn btn-primary ml-3">ไม่กดละ {{lable}}</button>
+  </div>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      counter: 0,
+    };
+  },
+  props: {
+    lable: String,
+  },
+  methods: {
+    upFunction: function () {
+      this.counter += 10;
+      this.$emit("cc", this.counter);
+    },
+
+    downFunction: function () {
+      this.counter += -5;
+      this.$emit("cc", this.counter);
+    },
+
+    LoveFunction: function () {
+      this.counter += 10;
+      this.$emit("cc", this.counter);
+    },
+
+    ResetFunction: function () {
+      this.counter = 0;
+      this.$emit("cc", this.counter);
+    },
+  },
+};
+</script>
+<style>
+</style>
